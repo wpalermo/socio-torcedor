e85a0caa567e5a04f7838b6f1fd5a82a76414448
William Palermo <palermow@gmail.com>
2018-04-10 23:05:29 -0300

    remocao de warnings

--- prova/campanha-service/src/main/java/com/wpalermo/campanha/controller/CampanhaRestController.java
+++ prova/campanha-service/src/main/java/com/wpalermo/campanha/controller/CampanhaRestController.java
@@ -8,7 +8,6 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
--- prova/campanha-service/src/test/java/com/wpalermo/campanha/CampanhaControllerTeste.java
+++ prova/campanha-service/src/test/java/com/wpalermo/campanha/CampanhaControllerTeste.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
