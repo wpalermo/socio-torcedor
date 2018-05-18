d5ed18a9f297c6fd82abbf9609d51907b6d75e06
William Palermo <william.palermo@accenture.com>
2018-05-18 16:54:53 -0300

    apanhado do eureka.. =/

--- eureka-service/src/main/java/com/wpalermo/eurekaService/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/wpalermo/eurekaService/EurekaServiceApplication.java
@@ -3,9 +3,7 @@ package com.wpalermo.eurekaService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource(value="file:./application.yml ")
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
