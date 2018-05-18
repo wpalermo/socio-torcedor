89c66a64402b85f10682e9bad14ffd7c5e4d0bf7
William Palermo <william.palermo@accenture.com>
2018-05-18 10:03:12 -0300

    mantendo apenas uma versao do projeto

--- prova-jdk10/socioTorcedor-service-jdk10/src/main/java/com/wpalermo/socioTorcedor/Application.java
+++ prova-jdk10/socioTorcedor-service-jdk10/src/main/java/com/wpalermo/socioTorcedor/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.PropertySource;
 
 
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrix
-@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
