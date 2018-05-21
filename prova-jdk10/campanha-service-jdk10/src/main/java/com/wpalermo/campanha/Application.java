f02efd61f147f08293e473ac0f6b0ca3ff98b90e
William Palermo <palermow@gmail.com>
2018-05-21 20:45:37 -0300

    eureka funcionando (falta nome do campanha no app name para o console do eureka)

--- prova-jdk10/campanha-service-jdk10/src/main/java/com/wpalermo/campanha/Application.java
+++ prova-jdk10/campanha-service-jdk10/src/main/java/com/wpalermo/campanha/Application.java
@@ -3,6 +3,7 @@ package com.wpalermo.campanha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.PropertySource;
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.PropertySource;
 @EnableCircuitBreaker
 @EnableHystrix
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
--- prova-jdk10/socioTorcedor-service-jdk10/src/main/java/com/wpalermo/socioTorcedor/Application.java
+++ prova-jdk10/socioTorcedor-service-jdk10/src/main/java/com/wpalermo/socioTorcedor/Application.java
@@ -3,6 +3,7 @@ package com.wpalermo.socioTorcedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.PropertySource;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrix
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
