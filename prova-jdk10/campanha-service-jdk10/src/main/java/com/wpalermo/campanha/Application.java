eec4aae4998112cd505a5926a1ade2bb3999d749
William Palermo <palermow@gmail.com>
2018-05-21 21:55:48 -0300

    exemplo do netflix feign (vantagens)

--- prova-jdk10/campanha-service-jdk10/src/main/java/com/wpalermo/campanha/Application.java
+++ prova-jdk10/campanha-service-jdk10/src/main/java/com/wpalermo/campanha/Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
 
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.PropertySource;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) {
