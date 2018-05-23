8b7eae6b4ae81f5af7ea15bf00448692abc6f984
William Palermo <palermow@gmail.com>
2018-05-22 22:20:30 -0300

    eureka + zuul + hystrix dashboard .. estudar mais feign

--- HystrixDashboard/src/main/java/com/wpalermo/HystrixDashboard/App.java
+++ HystrixDashboard/src/main/java/com/wpalermo/HystrixDashboard/App.java
@@ -3,11 +3,13 @@ package com.wpalermo.HystrixDashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Hello world!
  *
  */
+@PropertySource(value="file:./application.yml")
 @SpringBootApplication
 @EnableHystrixDashboard
 public class App 
